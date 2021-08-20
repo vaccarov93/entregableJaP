fbcdf4c35861bb561de4d3677baabf812edb716b
vaccarov93 <vaccarov.93@gmail.com>
2021-08-20 16:14:53 -0300

    Se agrega login con sus validaciones, la pag principal pasa a ser home.html

--- js/login.js
+++ js/login.js
@@ -2,5 +2,19 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    document.getElementById("btn").addEventListener("click", function(e){
+       
+        const mail= document.getElementById("correo");
+        const pwd = document.getElementById("contrasena");
+        
+        if(mail.value === ""){
+            alert("Hola!!, parece que te falta poner tu mail");
+        }
+        else if(pwd.value ===""){
+            alert("Hola!!, parece que te falta poner tu contraseña");
+        }
+        else{
+            window.location.href = "./home.html";
+        };
+    });
 });
