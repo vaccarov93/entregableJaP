9b3679333e342efa79706fbaaec8507a6b43120c
vaccarov93 <vaccarov.93@gmail.com>
2021-08-21 18:26:40 -0300

    se hacen las modificaciones para que en producto se listen los productos con su info pertinente

--- js/products.js
+++ js/products.js
@@ -3,4 +3,44 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
 
+    let url = "https://japdevdep.github.io/ecommerce-api/product/all.json"
+
+
+
+        fetch(url)
+            .then(content => content.json())
+            .then(data =>{
+                
+                for(let i=0;i < data.length; i ++){
+                    let datos = data[i];
+                    let name = datos.name;
+                    let description = datos.description;
+                    let cost = datos.cost;
+                    let currency = datos.currency;
+                    let imgUrl = datos.imgSrc;
+                    let sold = datos.soldCount;
+
+                    let agregar = "";
+
+                    agregar += `				
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="` + imgUrl + `" class="img-thumbnail">;
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">`+ name +`</h4>
+                                <big class="text-muted">` + currency+` `+cost + `</big>                    
+                            </div>
+                            <h7 class="d-flex w-100 justify-content-between">
+                                `+ description + `<small>` + sold + ` artículos</small>
+                            </h7>
+                        </div>
+                    </div>         
+                    `
+                    document.getElementById("contenido").innerHTML += agregar;
+                };      
+            });
+
+
 });
